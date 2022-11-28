8585382e11e89fbc13ea4cedfe10f1f99efd10e4
Umamahii <117980082+Umamahii@users.noreply.github.com>
2022-11-28 11:21:42 +0530

    Add files via upload

---
+++ database1.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from tkinter import messagebox
+import mysql.connector
+import os
+import time
+
+#connecting to the database
+db = mysql.connector.connect(host="localhost",user="root",password="admin",database="demo1")
+mycur = db.cursor()
+
+def error_destroy():
+    err.destroy()
+
+def succ_destroy():
+    succ.destroy()
+    root1.destroy()
+
+def error():
+    global err
+    err = Toplevel(root1)
+    err.title("Error")
+    err.geometry("200x100")
+    Label(err,text="All fields are required..",fg="red",font="bold").pack()
+    Label(err,text="").pack()
+    Button(err,text="Ok",bg="grey",width=8,height=1,command=error_destroy).pack()
+
+def success():
+    global succ
+    succ = Toplevel(root1)
+    succ.title("Success")
+    succ.geometry("200x100")
+    Label(succ, text="Registration successful...", fg="green", font="bold").pack()
+    Label(succ, text="").pack()
+    Button(succ, text="Ok", bg="grey", width=8, height=1, command=succ_destroy).pack()
+
+def register_user():
+    username_info = username.get()
+    password_info = password.get()
+    if username_info == "":
+        error()
+    elif password_info == "":
+        error()
+    else:
+        sql = "insert into login values(%s,%s)"
+        t = (username_info, password_info)
+        mycur.execute(sql, t)
+        db.commit()
+        Label(root1, text="").pack()
+        time.sleep(0.50)
+        success()
